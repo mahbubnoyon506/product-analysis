d2b3b68ad2719da5f8a9779425fba1e67a3c0b6c
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-04-04 14:25:51 +0600

    initial setup done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-wrap'>
+            <NavLink to="/" className={({ isActive }) => isActive ? "active" : "in-active"}>Home</NavLink>
+            <NavLink to="/reviews" className={({ isActive }) => isActive ? "active" : "in-active"}>Reviews</NavLink>
+            <NavLink to="/dashboard" className={({ isActive }) => isActive ? "active" : "in-active"}>Dashboard</NavLink>
+            <NavLink to="/blogs" className={({ isActive }) => isActive ? "active" : "in-active"}>Blogs</NavLink>
+            <NavLink to="/about" className={({ isActive }) => isActive ? "active" : "in-active"}>About</NavLink>
+        </div>
+    );
+};
+
+export default Header;
