9d86c8a7e02b4c2e06c3a7ccb561e9ea3fdc0074
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-04-04 20:09:06 +0600

    Initially done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,21 +6,22 @@ import User from '../User/User';
 import car from '../../images/car.jpg'
 
 const Home = () => {
-    const [users, setUser] = useUser();
+    const [users] = useUser();
     // console.log(users);
     return (
-        <div>
+        <div className='home-wrapper'>
             <div className="home-content">
                 <div className="content">
-                    <h2></h2>
-                    <h4></h4>
-                    <p></p>
+                    <h1>2023 Audi A6 e-tron</h1>
+                    <h3><span>Starting at</span>$80,000</h3>
+                    <p>Audi continues its electric-vehicle assault with the A6 e-tron sedan, which is aimed directly at the Tesla Model S.</p>
                 </div>
                 <img src={car} alt="" />
             </div>
+            <h2>Customer Reviews</h2>
             <div className="users-wrap">
                 {
-                    users.map(user => <User key={user.id} user={user}></User>)
+                   users.length > 3 ? users.map(user => <User key={user.id} user={user}></User>) : users.map(user => <User key={user.id} user={user}></User>)
                 }
             </div>
             <Link className='show-all-btn' to='/reviews'>All Reviews</Link>
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Reviews.css'
-import { Link } from 'react-router-dom';
 import useUser from '../Hooks/CustomHook';
 import User from '../User/User';
 
 const Reviews = () => {
-    const [users, setUser] = useUser();
+    const [users] = useUser();
     // console.log(users);
     return (
-        <div>
+        <div className='review-wrapper'>
+            <h1>What Our Customer Say!</h1>
             <div className="review-wrap">
                 {
                     users.map(user => <User key={user.id} user={user}></User>)
